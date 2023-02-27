be3473ef3b21bb20a70000779d7be33d964e43db
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-27 09:08:48 -0800

    this is the problem solving

--- advance.js
+++ advance.js
@@ -1,4 +1,18 @@
 const numbers = [1, 3, 5, 7, 9]
 
-const number = numbers.map (n => n +1 );
-console.log(number)
+const number = numbers.map(n => n + 1);
+// console.log(number)
+
+let data = {
+    Sophia: {
+        id: 33,
+        study: [
+            {
+                primary:
+                    [ { school_name: "ABC primary school" },  { location: "Peters burg" }
+                    ]
+            },{  secondary:
+                    [ {  school_name: "ABC secondary school" },{ location: "St Lorence" } ] },
+        ]
+    }
+}
